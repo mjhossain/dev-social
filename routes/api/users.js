62288bdb73753fedcb27b00efa011376861f68b0
mjhossain <mjhossainnyc@gmail.com>
2020-07-06 16:03:29 -0400

    updates

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator/check')
 
-// @route       GET api/users
+// @route       Post api/users
 // @desc        Test router
 // @access      Public
-router.get('/', [
+router.post('/', [
     check('name', 'Name is required!').not().isEmpty(),
     check('email', 'Please enter a valid email.').isEmail(),
     check('password', 'Please enter a password with atlease 6 characters.').isLength({ min: 6 })
