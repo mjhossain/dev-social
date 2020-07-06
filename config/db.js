e03a0d230e328cd15e783421e7a392f3386ee42e
mjhossain <mjhossainnyc@gmail.com>
2020-07-06 14:26:37 -0400

    mongoDB connected

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const config = require('config')
+
+const dbURL = config.get('mongoURI')
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(dbURL, { useNewUrlParser: true })
+        console.log('Database Connected')
+    } catch (err) {
+        console.error(err.message)
+            // Exit App
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
