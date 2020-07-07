316b8c354092995ed4ca19f832dcbc2bb5eceebe
mjhossain <mjhossainnyc@gmail.com>
2020-07-07 14:10:45 -0400

    updates

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
 
-// @route       GET api/profile
-// @desc        Test router
+const Profile = require('../../models/Profile')
+
+
+// @route       GET api/profile/me
+// @desc        Get User Profile
 // @access      Public
-router.get('/', (req, res) => {
-    res.send('Profile route')
+router.get('/me', auth, async(req, res) => {
+    try {
+        const profile = await await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar'])
+        if (!profile) {
+            return res.status(400).json({ msg: 'Profile not found!' })
+        }
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server error')
+    }
 })
 
 module.exports = router
